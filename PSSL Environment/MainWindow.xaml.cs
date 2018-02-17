7ba6c16e8d4989c0d327554256dce6965fc9806d
Jamie Briggs <jamie.briggs@hotmail.co.uk>
2018-02-17 21:22:16 +0000

    Swapped viewport to use SharpGL instead of Helix3D due to wanting to use OpenGL within the project instead of DirectX.

--- PSSL Environment/MainWindow.xaml.cs
+++ PSSL Environment/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Media.Media3D;
+using SharpGL;
 using Microsoft.Win32;
 
 namespace PSSL_Environment
@@ -35,111 +36,111 @@ namespace PSSL_Environment
             //device3D.Content = Display3d("C:\\Users\\jamie\\OneDrive\\Individual Project\\Project\\PSSL Environment\\PSSL Environment\\Resources\\Models\\cube.obj");
         }
 
-        public Model3DGroup load3dModel(string path)
-        {
-            ObjReader CurrentHelixObjReader = new ObjReader();
-            // Model3DGroup MyModel = CurrentHelixObjReader.Read(@"D:\3DModel\dinosaur_FBX\dinosaur.fbx");
-            // Model3DGroup MyModel = CurrentHelixObjReader.Read(@"C:\Users\aaa\Downloads\jlb4kmi4xssg-iphone6model\iphone_6_model.FBX");
+        //public Model3DGroup load3dModel(string path)
+        //{
+        //    ObjReader CurrentHelixObjReader = new ObjReader();
+        //    // Model3DGroup MyModel = CurrentHelixObjReader.Read(@"D:\3DModel\dinosaur_FBX\dinosaur.fbx");
+        //    // Model3DGroup MyModel = CurrentHelixObjReader.Read(@"C:\Users\aaa\Downloads\jlb4kmi4xssg-iphone6model\iphone_6_model.FBX");
 
 
-            Model3DGroup model = null;
+        //    Model3DGroup model = null;
 
-            string ext = System.IO.Path.GetExtension(path).ToLower();
-            switch (ext)
-            {
-                case ".3ds":
-                    {
-                        var r = new StudioReader();
-                        model = r.Read(path);
-                        break;
-                    }
+        //    string ext = System.IO.Path.GetExtension(path).ToLower();
+        //    switch (ext)
+        //    {
+        //        case ".3ds":
+        //            {
+        //                var r = new StudioReader();
+        //                model = r.Read(path);
+        //                break;
+        //            }
 
-                case ".fbx":
-                    {
-                        var r = new HelixToolkit.Wpf.ObjReader();
-                        model = r.Read(path);
-                        break;
-                    }
+        //        case ".fbx":
+        //            {
+        //                var r = new HelixToolkit.Wpf.ObjReader();
+        //                model = r.Read(path);
+        //                break;
+        //            }
 
-                case ".lwo":
-                    {
-                        var r = new HelixToolkit.Wpf.LwoReader();
-                        model = r.Read(path);
+        //        case ".lwo":
+        //            {
+        //                var r = new HelixToolkit.Wpf.LwoReader();
+        //                model = r.Read(path);
 
-                        break;
-                    }
+        //                break;
+        //            }
 
-                case ".obj":
-                    {
-                        var r = new HelixToolkit.Wpf.ObjReader();
-                        model = r.Read(path);
+        //        case ".obj":
+        //            {
+        //                var r = new HelixToolkit.Wpf.ObjReader();
+        //                model = r.Read(path);
 
-                        //Material matty = (MaterialGroup)((GeometryModel3D)model.Children[0]).Material;
-                        //Material myMaterial = MaterialHelper.CreateImageMaterial(@"C:\Users\aaa\Downloads\jlb4kmi4xssg-iphone6model\sam-scrn.jpg", 1);
-                        // Material anotherMaterial = ((GeometryModel3D)model.Children[0]).Material;
-                        //  Newmodel.Children.Add(new GeometryModel3D { Geometry = anotherMaterial, Material = myMaterial });
-                        break;
-                    }
+        //                //Material matty = (MaterialGroup)((GeometryModel3D)model.Children[0]).Material;
+        //                //Material myMaterial = MaterialHelper.CreateImageMaterial(@"C:\Users\aaa\Downloads\jlb4kmi4xssg-iphone6model\sam-scrn.jpg", 1);
+        //                // Material anotherMaterial = ((GeometryModel3D)model.Children[0]).Material;
+        //                //  Newmodel.Children.Add(new GeometryModel3D { Geometry = anotherMaterial, Material = myMaterial });
+        //                break;
+        //            }
 
-                case ".objz":
-                    {
-                        var r = new HelixToolkit.Wpf.ObjReader();
-                        model = r.ReadZ(path);
-                        break;
-                    }
+        //        case ".objz":
+        //            {
+        //                var r = new HelixToolkit.Wpf.ObjReader();
+        //                model = r.ReadZ(path);
+        //                break;
+        //            }
 
-                case ".stl":
-                    {
-                        var r = new HelixToolkit.Wpf.StLReader();
-                        model = r.Read(path);
-                        break;
-                    }
+        //        case ".stl":
+        //            {
+        //                var r = new HelixToolkit.Wpf.StLReader();
+        //                model = r.Read(path);
+        //                break;
+        //            }
 
-                case ".off":
-                    {
-                        var r = new HelixToolkit.Wpf.OffReader();
-                        model = r.Read(path);
-                        break;
-                    }
+        //        case ".off":
+        //            {
+        //                var r = new HelixToolkit.Wpf.OffReader();
+        //                model = r.Read(path);
+        //                break;
+        //            }
 
-                default:
-                    throw new InvalidOperationException("File format not supported.");
-            }
+        //        default:
+        //            throw new InvalidOperationException("File format not supported.");
+        //    }
 
-            return model;
-            //MyModel.Children.Add(MyModel);
+        //    return model;
+        //    //MyModel.Children.Add(MyModel);
 
 
-        }
+        //}
 
-        /// <summary>
-        /// Display 3D Model
-        /// </summary>
-        /// <param name="model">Path to the Model file</param>
-        /// <returns>3D Model Content</returns>
-        private Model3D Display3d(string model)
-        {
-            Model3D device = null;
-            try
-            {
-                //Adding a gesture here
-                viewPort3d.RotateGesture = new MouseGesture(MouseAction.LeftClick);
-                //Adding a scale here
-                viewPort3d.ZoomGesture = new MouseGesture(MouseAction.RightClick);
+        ///// <summary>
+        ///// Display 3D Model
+        ///// </summary>
+        ///// <param name="model">Path to the Model file</param>
+        ///// <returns>3D Model Content</returns>
+        //private Model3D Display3d(string model)
+        //{
+        //    Model3D device = null;
+        //    try
+        //    {
+        //        ////Adding a gesture here
+        //        //viewPort3d.RotateGesture = new MouseGesture(MouseAction.LeftClick);
+        //        ////Adding a scale here
+        //        //viewPort3d.ZoomGesture = new MouseGesture(MouseAction.RightClick);
 
-                //Import 3D model file
-                ModelImporter import = new ModelImporter();
+        //        //Import 3D model file
+        //        ModelImporter import = new ModelImporter();
 
-                //Load the 3D model file
-                device = load3dModel(model);
-            }
-            catch (Exception e)
-            {
-                // Handle exception in case can not file 3D model
-                MessageBox.Show("Exception Error : " + e.StackTrace);
-            }
-            return device;
-        }
+        //        //Load the 3D model file
+        //        device = load3dModel(model);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        // Handle exception in case can not file 3D model
+        //        MessageBox.Show("Exception Error : " + e.StackTrace);
+        //    }
+        //    return device;
+        //}
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
@@ -178,7 +179,7 @@ namespace PSSL_Environment
             byte g = singleColorCanvas.G;
             byte b = singleColorCanvas.B;
             byte a = singleColorCanvas.A;
-            Helix3D_Viewport.Background = new SolidColorBrush(Color.FromArgb(a, r, g, b));
+            Viewport.Background = new SolidColorBrush(Color.FromArgb(a, r, g, b));
         }
 
         private void btnLoad_Click(object sender, RoutedEventArgs e)
@@ -197,181 +198,141 @@ namespace PSSL_Environment
                 imgPhoto.Source = new BitmapImage(new Uri(op.FileName));
             }
         }
-        //    private void RadioButton_Unchecked(object sender, RoutedEventArgs e)
-        //    {
-        //        RadioButton rb = sender as RadioButton;
 
-
-        //        if (rb != null)
-        //        {
-        //            string colorName = rb.Tag.ToString();
-        //            switch (colorName)
-        //            {
-        //                case "singleColour":
-        //                    PixelShaderGrid.Children.Remove(visualBoard);
-        //                    //lgb.
-        //                    break;
-        //                case "textureColour":
-        //                    PixelColourOptionTitle.Text = "Texture";
-
-        //                    break;
-        //                case "templateColour":
-        //                    PixelColourOptionTitle.Text = "Template";
-        //                    break;
-        //            }
-        //        }
-        //        else
-        //        {
-        //            PixelColourOptionTitle.Text = "Error";
-        //        }
-        //    }
-
-        //    // Visual Board for colours
-        //    Rectangle visualBoard = new Rectangle();
-
-        //    public void CreateVisualBrush()
-        //    {
-        //        // Create a background recntangle
-
-
-
-        //        visualBoard.Width = 300;
-
-        //        visualBoard.Height = 300;
-
-
-
-        //        // Create a DrawingBrush
-
-        //        VisualBrush vBrush = new VisualBrush();
-
-
-
-        //        // Create a StackPanel and add a few controls to it
-
-        //        StackPanel stkPanel = new StackPanel();
-
-
-
-        //        // Create a Rectangle and add it to StackPanel
-
-        //        Rectangle Rectangle = new Rectangle();
-
-        //        Rectangle.Height = 100;
-
-        //        Rectangle.Width = 20;
-
-        //        // Linear Gradient Brush Settings!!!
-        //        //
-        //        //
-        //        LinearGradientBrush HueBrush = new LinearGradientBrush();
-
-        //        HueBrush.StartPoint = new Point(0, 0);
-
-        //        HueBrush.EndPoint = new Point(0, 1);
-
-
-        //        // RedStart Value
-        //        GradientStop RedStart = new GradientStop();
-
-        //        RedStart.Color = Color.FromRgb(255, 0, 0);
-
-        //        RedStart.Offset = 0.0;
-
-        //        HueBrush.GradientStops.Add(RedStart);
-
-
-        //        // Yellow Value
-        //        GradientStop Yellow = new GradientStop();
-
-        //        Yellow.Color = Color.FromRgb(255, 255, 0);
-
-        //        Yellow.Offset = 0.167;
-
-        //        HueBrush.GradientStops.Add(Yellow);
-
-
-        //        // Green Value
-        //        GradientStop Green = new GradientStop();
-
-        //        Green.Color = Color.FromRgb(0, 255, 0);
-
-        //        Green.Offset = 0.333;
-
-        //        HueBrush.GradientStops.Add(Green);
-
-
-        //        // LightBlue Value
-        //        GradientStop LightBlue = new GradientStop();
-
-        //        LightBlue.Color = Color.FromRgb(0, 255, 255);
-
-        //        LightBlue.Offset = 0.5;
-
-        //        HueBrush.GradientStops.Add(LightBlue);
-
-
-        //        // Blue Value
-        //        GradientStop Blue = new GradientStop();
-
-        //        Blue.Color = Color.FromRgb(0, 0, 255);
-
-        //        Blue.Offset = 0.667;
-
-        //        HueBrush.GradientStops.Add(Blue);
-
-
-        //        // Magenta Value
-        //        GradientStop Magenta = new GradientStop();
-
-        //        Magenta.Color = Color.FromRgb(255, 0, 255);
-
-        //        Magenta.Offset = 0.833;
-
-        //        HueBrush.GradientStops.Add(Magenta);
-
-        //        // RedFinish Value
-        //        GradientStop RedFinish = new GradientStop();
-
-        //        RedFinish.Color = Color.FromRgb(255, 0, 0);
-
-        //        RedFinish.Offset = 1.0;
-
-        //        HueBrush.GradientStops.Add(RedFinish);
-
-
-        //        Rectangle.Fill = HueBrush;
-
-
-
-        //        stkPanel.Children.Add(Rectangle);
-
-        //        ///////////////////////////////////////////////
-
-
-
-        //        // Set Viewport and TileMode
-        //        vBrush.Viewport = new Rect(0, 0, 0.25, 0.25);
-        //        vBrush.TileMode = TileMode.None;
-
-
-
-        //        // Set Visual of VisualBrush
-        //        vBrush.ca
-        //        vBrush.Visual = stkPanel;
-
-
-
-        //        // Fill rectangle with a DrawingBrush
-
-        //        visualBoard.Fill = vBrush;
-
-
-
-        //        PixelShaderGrid.Children.Add(visualBoard);
-        //    }
-
-        //}
+        private void OpenGLControl_OpenGLDraw(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
+        {
+            //  Get the OpenGL instance that's been passed to us.
+            OpenGL gl = args.OpenGL;
+
+            //  Clear the color and depth buffers.
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            //  Reset the modelview matrix.
+            gl.LoadIdentity();
+
+            //  Move the geometry into a fairly central position.
+            gl.Translate(-1.5f, 0.0f, -6.0f);
+
+            //  Draw a pyramid. First, rotate the modelview matrix.
+            gl.Rotate(rotatePyramid, 0.0f, 1.0f, 0.0f);
+
+            //  Start drawing triangles.
+            gl.Begin(OpenGL.GL_TRIANGLES);
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0.0f, 1.0f, 0.0f);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0.0f, 1.0f, 0.0f);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0.0f, 1.0f, 0.0f);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0.0f, 1.0f, 0.0f);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+
+            gl.End();
+
+            //  Reset the modelview.
+            gl.LoadIdentity();
+
+            //  Move into a more central position.
+            gl.Translate(1.5f, 0.0f, -7.0f);
+
+            //  Rotate the cube.
+            gl.Rotate(rquad, 1.0f, 1.0f, 1.0f);
+
+            //  Provide the cube colors and geometry.
+            gl.Begin(OpenGL.GL_QUADS);
+
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(1.0f, 1.0f, -1.0f);
+            gl.Vertex(-1.0f, 1.0f, -1.0f);
+            gl.Vertex(-1.0f, 1.0f, 1.0f);
+            gl.Vertex(1.0f, 1.0f, 1.0f);
+
+            gl.Color(1.0f, 0.5f, 0.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(1.0f, 1.0f, 1.0f);
+            gl.Vertex(-1.0f, 1.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+
+            gl.Color(1.0f, 1.0f, 0.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Vertex(-1.0f, 1.0f, -1.0f);
+            gl.Vertex(1.0f, 1.0f, -1.0f);
+
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(-1.0f, 1.0f, 1.0f);
+            gl.Vertex(-1.0f, 1.0f, -1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+
+            gl.Color(1.0f, 0.0f, 1.0f);
+            gl.Vertex(1.0f, 1.0f, -1.0f);
+            gl.Vertex(1.0f, 1.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+
+            gl.End();
+
+            //  Flush OpenGL.
+            gl.Flush();
+
+            //  Rotate the geometry a bit.
+            rotatePyramid += 3.0f;
+            rquad -= 3.0f;
+        }
+
+        float rotatePyramid = 0;
+        float rquad = 0;
+
+        private void OpenGLControl_OpenGLInitialized(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
+        {
+            //  Enable the OpenGL depth testing functionality.
+            args.OpenGL.Enable(OpenGL.GL_DEPTH_TEST);
+        }
+
+        private void OpenGLControl_Resized(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
+        {
+            // Get the OpenGL instance.
+            OpenGL gl = args.OpenGL;
+
+            // Load and clear the projection matrix.
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            gl.LoadIdentity();
+
+            // Perform a perspective transformation
+            gl.Perspective(45.0f, (float)gl.RenderContextProvider.Width /
+                (float)gl.RenderContextProvider.Height,
+                0.1f, 100.0f);
+
+            // Load the modelview.
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+        }
 
 
     }
