b84d91eed35f7a509ed4da240c2a30cf0a706b95
Jamie Briggs <jamie.briggs@hotmail.co.uk>
2018-03-25 02:11:18 +0100

    More changes to interpreting vectors.

--- PSSL Environment/Interpreter.cs
+++ PSSL Environment/Interpreter.cs
@@ -21,6 +21,10 @@ namespace PSSL_Environment
 
         private string ShaderName;
 
+        private string ShaderVertexFilename;
+        private string ShaderConstantsFileName;
+        private string ShaderOutputFileName;
+
         private Interpreter() { }
 
         private enum Type { MAT4, VEC3, VEC4, FLOAT};
@@ -142,6 +146,7 @@ namespace PSSL_Environment
             SetShaderName("test");
             GeneratePSSLConstantsFile();
             GeneratePSSLOutputFile();
+            GeneratePSSLVertexFile(Vert);
         }
 
         public void GeneratePSSLConstantsFile()
@@ -201,6 +206,8 @@ namespace PSSL_Environment
             string path = filePath + "\\" + ShaderName + "ShaderConstants.h";
             System.IO.File.WriteAllText(path, output);
 
+            ShaderConstantsFileName = ShaderName + "ShaderConstants.h";
+
         }
 
         public void GeneratePSSLOutputFile()
@@ -243,11 +250,79 @@ namespace PSSL_Environment
             //System.IO.File.WriteAllText(@"Shaders\Generated\" + ClassName + "ShaderConstents.h", output);
             string path = filePath + "\\" + ShaderName + "VSOutput.hs";
             System.IO.File.WriteAllText(path, output);
+
+            ShaderOutputFileName = ShaderName + "VSOutput.hs";
         }
 
-        //public void GeneratePSSLVertexFile(string vert)
-        //{
-        //    string PSSLVert = "// " + ShaderName + " Vertex Shader" + Environment.NewLine;
-        //}
+        public void GeneratePSSLVertexFile(string vert)
+        {
+            string PSSLVert = "// " + ShaderName + " Vertex Shader" + Environment.NewLine;
+            
+            // Add all includes to top of the file
+            PSSLVert = PSSLVert + "#include \"" + ShaderConstantsFileName 
+                + "\"" + Environment.NewLine;
+            PSSLVert = PSSLVert + "#include \"" + ShaderOutputFileName
+                + "\"" + Environment.NewLine;
+            PSSLVert = PSSLVert + Environment.NewLine;
+
+            // Add ptVSInput struct to top
+            PSSLVert = PSSLVert + "struct ptVSInput" + Environment.NewLine;
+            PSSLVert = PSSLVert + "{" + Environment.NewLine;
+            PSSLVert = PSSLVert + "\tfloat3 m_position\t\t: POSITION0;" + Environment.NewLine;
+            PSSLVert = PSSLVert + "\tfloat3 m_color\t\t\t: COLOR0;" + Environment.NewLine;
+            PSSLVert = PSSLVert + "\tfloat3 m_normal\t\t: NORMAL0;" + Environment.NewLine;
+            PSSLVert = PSSLVert + "\tfloat2 m_texcoord\t\t: TEXCOORD0;" + Environment.NewLine;
+            PSSLVert = PSSLVert + "};" + Environment.NewLine;
+
+            // Add space
+            PSSLVert = PSSLVert + Environment.NewLine;
+
+            //using (StringReader reader = new StringReader(vert))
+            //{
+            //    string line = string.Empty;
+            //    do
+            //    {
+            //        // Ensures to skip any line containing parameters passed in as they are
+            //        //  already dealt with
+            //        line = reader.ReadLine();
+            //        if (line != null && !line.Contains("in")
+            //            && !line.Contains("out") && !line.Contains("uniform") 
+            //            && !line.Contains("varying") && !line.Contains("precision")
+            //            && !line.Contains("#"))
+            //        {
+            //            string[] temp = line.Split(new char[] { ' ', ';' });
+            //            //line.Trim(new char[] { ' ', ';' });
+            //            //line.Replace("uniform ", "");
+            //            if (temp[1].Contains("vec3"))
+            //            {
+            //                //line.Replace("vec3", "" );
+            //                Constants.Add(new KeyValuePair<Type, string>(Type.VEC3, temp[2]));
+            //            }
+            //            else if (temp[1].Contains("mat4"))
+            //            {
+            //                //line.Replace("vec3", "" );
+            //                Constants.Add(new KeyValuePair<Type, string>(Type.MAT4, temp[2]));
+            //            }
+            //            else if (temp[1].Contains("float"))
+            //            {
+            //                //line.Replace("vec3", "" );
+            //                Constants.Add(new KeyValuePair<Type, string>(Type.FLOAT, temp[2]));
+            //            }
+            //        }
+
+            //    } while (line != null);
+            //}
+
+            // WRITE TO FILE
+            Encoding utf8 = Encoding.UTF8;
+            Encoding ascii = Encoding.ASCII;
+
+            string output = ascii.GetString(Encoding.Convert(utf8, ascii, utf8.GetBytes(PSSLVert)));
+            //System.IO.File.WriteAllText(@"Shaders\Generated\" + ClassName + "ShaderConstents.h", output);
+            string path = filePath + "\\" + ShaderName + "_vv.pssl";
+            System.IO.File.WriteAllText(path, output);
+
+            ShaderVertexFilename = ShaderName + "_vv.pssl";
+        }
     }
 }
